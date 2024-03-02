9dcca714262cf1705432e1ab11d84474c28630bf
Lobsang <losbsang.physics@gmail.com>
2024-03-01 18:56:59 -0600

    nuevos nombres y primer parcial

---
+++ Parciales/parcial_1.py
@@ -0,0 +1,28 @@
+# File: parcial_1
+# Date: 01/03/2024
+
+#importar Librerias
+
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import math 
+import streamlit as st
+
+#Ponemos el título de la Práctica
+
+st.title('Examen Parcial 1 - Parte Práctica')
+st.subheader('por Lobsang Méndez - 202112428')
+
+
+
+
+
+
+
+
+
+
+
+#ejecutamos nuestra página de Streamlit
+
---
+++ Practicas/p1.py
@@ -0,0 +1,7 @@
+#File: pi.py
+#date: 26/02/2024
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
