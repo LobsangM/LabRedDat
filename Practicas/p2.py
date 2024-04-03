e00b0e763001b27e701fc04a486de5c20e751237
Lobsang <losbsang.physics@gmail.com>
2024-04-03 16:34:25 -0600

    primera grafica practica 2

--- Practicas/p2.py
+++ Practicas/p2.py
@@ -4,21 +4,52 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import scipy as scy
+from scipy import stats as sst
+from scipy import optimize as sco
 import math
 import plotly.express as px
+import matplotlib.pyplot as plt
 
+#Cargar el archivo csv y hacer que Pandas lea el archivo
 data = pd.read_csv('confirmados_fecha.csv')
-print(f'data:\n{data}')
+#print(f'data:\n{data}')
 
 
+#definimos una lista que será que lea scypi
 lista_fechas = []
 
-fila_max = 100
+#definimos una variable indice que será hasta que fila vamos a leer
+fila_max = 98
 
+#convertimos el archivo csv en una lista 
 for index, row in data.iterrows():
     if index <= fila_max:
 
         lista_fechas.extend([row['fecha']] * row['Casos por fecha de inicio de síntomas'])
 
-print(lista_fechas)    
+#print(lista_fechas)    
+
+
+def grafica(lista_fechas):
+
+    #definimos los ejes del histigrama y tomamos su frecuencia 
+    date, counts = np.unique(lista_fechas, return_counts=True)
+
+    #Configurar la figura y el histograma
+    plt.figure(figsize=(10, 6))
+    plt.bar(date, counts, color='blue')
+    plt.title('Histograma de Fechas Casos Covid 19')
+    plt.xlabel('Fechas')
+    plt.ylabel('Casos')
+    plt.xticks(rotation=90)
+
+    return plt
+
+
+#Streamlit
+
+st.title('Histograma de Casos Covid')
+
+#Histograma
+
+st.pyplot(grafica(lista_fechas))
